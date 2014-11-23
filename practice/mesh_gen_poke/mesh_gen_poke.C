08a6c9788b64d2f9ee06f1bcfb9b8c76c919e2d1
Harriet <kameeko@gmail.com>
2014-11-23 11:09:27 -0500

    messing with subdomains of T channel

---
+++ practice/mesh_gen_poke/mesh_gen_poke.C
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+
+#include "libmesh/libmesh.h"
+#include "libmesh/mesh.h"
+#include "libmesh/mesh_generation.h"
+#include "libmesh/vtk_io.h"
+#include "libmesh/linear_implicit_system.h"
+#include "libmesh/equation_systems.h"
+#include "libmesh/mesh_refinement.h"
+
+#include "libmesh/fe.h"
+
+#include "libmesh/quadrature_gauss.h"
+
+#include "libmesh/sparse_matrix.h"
+#include "libmesh/numeric_vector.h"
+#include "libmesh/dense_matrix.h"
+#include "libmesh/dense_vector.h"
+#include "libmesh/elem.h"
+
+#include "libmesh/dof_map.h"
+
+#include "libmesh/exodusII_io.h"
+
+using namespace libMesh;
+
+int main (int argc, char** argv){
+
+	LibMeshInit init (argc, argv); //initialize libmesh library
+
+	std::cout << "Running " << argv[0];
+	for (int i=1; i<argc; i++)
+		std::cout << " " << argv[i];
+	std::cout << std::endl << std::endl;
+
+	Mesh mesh(init.comm());
+	mesh.read("mesh.e");
+	
+	MeshRefinement meshRefinement(mesh);
+	meshRefinement.uniformly_refine(3);
+
+	MeshBase::element_iterator       elem_it  = mesh.elements_begin();
+  const MeshBase::element_iterator elem_end = mesh.elements_end();
+  for (; elem_it != elem_end; ++elem_it){
+    Elem* elem = *elem_it;
+    double d = elem->centroid().size();
+    if(d<0.8)
+        elem->subdomain_id() = 1;
+    else
+    		elem->subdomain_id() = 2;
+  }
+
+
+	EquationSystems equation_systems (mesh);
+
+	#ifdef LIBMESH_HAVE_EXODUS_API
+    ExodusII_IO (mesh).write_equation_systems("meep.exo",equation_systems);
+  #endif // #ifdef LIBMESH_HAVE_EXODUS_API
+
+	return 0;
+} // end main
