861c58bfc596db96a70988c67c8a4370ed2fd8cc
Harriet <kameeko@gmail.com>
2015-05-16 00:31:04 -0400

    chicks

--- practice/T_channel/make_pretties.m
+++ practice/T_channel/make_pretties.m
@@ -1,13 +1,13 @@
 %to make pretty plots from error breakdown file
 close all;
 
-refLevel = 'MF06';
+refLevel = 'MF03';
 dir_root = ['',...
   'with_reaction/long_channel_stash/'...
-  '3by3_suite/qoi3_sens3/',refLevel,'/'];
+  '3by3_suite_fine/qoi3_sens3/',refLevel,'/'];
 
 errFileID = ...
-  fopen([dir_root,'error_est_breakdown_beta.dat'],'r');
+  fopen([dir_root,'error_est_breakdown.dat'],'r');
 errFormatSpec = '%f %f %f';
 errSizeMat = [3 Inf];
 
@@ -15,7 +15,9 @@ A = fscanf(errFileID,errFormatSpec,errSizeMat);
 A = A';
 
 err = figure(1); 
-scatter(A(:,1),A(:,2),300,A(:,3),'s','filled'); colorbar
+scatter(A(:,2),A(:,1),100,A(:,3),'s','filled'); colorbar
+% scatter(A(:,1),A(:,2),100,A(:,3),'s','filled'); colorbar
+% scatter(A(:,1),A(:,2),300,A(:,3),'s','filled'); colorbar
 % hold on
 % patch([0.625 1.5 1.5 0.625],...
 %   [0.25 0.25 0.75 0.75],'w',...
@@ -25,15 +27,33 @@ scatter(A(:,1),A(:,2),300,A(:,3),'s','filled'); colorbar
 %   'MarkerEdgeColor','w',...
 %   'LineWidth',3,...
 %   'MarkerFaceColor',[0 .7 .7]);
-xlabel('x'); ylabel('y'); 
-title('QoI Error Estimate Contribution')
+% xlabel('x'); 
+% ylabel('y'); 
+% set(gca,'xtick',[])
+% set(gca,'YTickMode','Manual','YTickLabelMode','Manual')
+set(gca,'ytick',[])
+ylim([0 5])
+% set(gca,'xtickLabel',{' ',' ',' '})
+xlabel('y','FontSize',14)
+set(gca,'XDir','Reverse')
+% set(gca,'ytickLabel',{' ',' ',' '})
+% xlabel('{ }'); 
+% ylabel('{ }'); 
+% title('Elemental Decomposition of QoI Error Estimate')
+% title('QoI Error Estimate Contribution')
 % title('{ }')
-set(gca,'FontSize',20); 
-set(findall(gcf,'type','text'),'FontSize',20)
-caxis([0 0.00004])
-set(gcf,'PaperPositionMode','auto','Position',[66 253 1535 297])
-print(err,[dir_root, 'err_breakdown_', refLevel,'_beta'],'-depsc');
-
+set(gca,'FontSize',14); 
+% meep=colorbar;
+caxis([0 0.000037])
+% set(meep,'FontSize',15)
+% set(findall(gcf,'type','text'),'FontSize',20)
+% set(gcf,'PaperPositionMode','auto','Position',[66 253 1535 297])
+% set(gcf,'PaperPositionMode','auto','Position',[66 253 1200 297])
+% set(gcf,'PaperPositionMode','auto','Position',[66 253 1200 333])
+set(gcf,'PaperPositionMode','auto','Position',[1 1 300/1.5 1150/4.1])
+% set(gcf,'PaperPositionMode','auto','Position',[1 1 300/1.5 1150/3.6])
+print(err,[dir_root, 'err_breakdown_', refLevel],'-depsc');
+break
 divFileID = ...
   fopen([dir_root,'divvy.txt'],'r');
 divFormatSpec = '%d %d';
@@ -55,12 +75,15 @@ divvy = figure(2);
 % scatter(A(LFbits,1),A(LFbits,2),270,[227, 38, 54]/255,'s','filled');
 % scatter(A(HFbits,1),A(HFbits,2),270,[91, 146, 229]/255,'s','filled');
 map = [91, 146, 229; 227, 38, 54]/255;
-squish = reshape(B,75,15)';
+% squish = reshape(B,75,15)';
+squish = reshape(B,250,50)';
 imagesc([0 5],[0 1],-squish); 
 set(gca,'YDir','normal','Ticklength',[0 0]); colormap(map);
-xlabel('{ }'); ylabel('{ }'); 
-title('{ }')
-set(gca,'FontSize',20); 
-set(findall(gcf,'type','text'),'FontSize',20)
-set(gcf,'PaperPositionMode','auto','Position',[66 253 1475 297])
+xlabel('x'); ylabel('y'); 
+title('Division of Domain')
+set(gca,'FontSize',32); 
+set(findall(gcf,'type','text'),'FontSize',32)
+% set(gcf,'PaperPositionMode','auto','Position',[66 253 1475 297])
+% set(gcf,'PaperPositionMode','auto','Position',[66 253 1150 297])
+set(gcf,'PaperPositionMode','auto','Position',[66 253 1150 333])
 print(divvy,[dir_root, 'cd_cdr_', refLevel, '_divvy'],'-depsc');
---
+++ practice/T_channel/make_setup.m
@@ -0,0 +1,51 @@
+%to create "setup" plots
+close all;
+
+ftsize = 13;
+
+fig_h = figure(1);
+% set(gcf,'Position',[1 1 1150 240])
+% set(gcf,'Position',[1 1 1150 300])
+set(gcf,'Position',[1 1 300/2.1 1150/4])
+axes1 = axes('Parent',fig_h,'FontSize',ftsize);
+hold on;
+
+%background
+% patch([0 5 5 0],[0 0 1 1],'w')
+patch([0 0 1 1], [0 5 5 0],'w')
+
+% %QoI region
+ptsx = [0.625 0.875 0.875 0.625]; %qoi3
+ptsy = [0.375 0.375 0.625 0.625]; %qoi3
+% ptsx = [2.375 2.625 2.625 2.375]; %qoi6
+% ptsy = [0.375 0.375 0.625 0.625]; %qoi6
+% ptsx = [0.625 1.5 1.5 0.625]; %qoi7
+% ptsy = [0.25 0.25 0.75 0.75]; %qoi7
+% ptsx = [0 5 5 0]; %qoi5
+% ptsy = [0 0 1 1]; %qoi5
+subreg = patch(ptsy, ptsx,...
+  [0.87843137254902 0.690196078431373 1], ...
+  'EdgeColor','none'); 
+
+%data points
+% ptsx = [0.35 1.56 3.1];
+% ptsy = [0.35 0.61 0.5];
+ptsx = [0.35 0.98 1.56 2.46 3.1];
+ptsy = [0.35 0.51 0.61 0.37 0.5];
+% % ptsx = [0.35 0.98 1.11 1.56 2.23 2.46 2.92 3.1 3.49 4.79];
+% % ptsy = [0.35 0.51 0.89 0.61 0.51 0.37 0.7 0.5 0.76 0.26];
+datapts = scatter(ptsy,ptsx,30, ...
+  'MarkerEdgeColor',[0 .5 .5],'MarkerFaceColor',[0 .7 .7]);
+
+% leg_h = legend([subreg datapts],{'\Omega_I','Observations'},...
+%   'Position',[0.776238418335466, 0.412506804633401,...
+%    0.217572463768116, 0.321180555555556]);
+% set(leg_h,'FontSize',20)
+
+ylabel('x','FontSize',ftsize);
+xlabel('{ }','FontSize',ftsize);
+% title('Location of Observations and QoI Region','FontSize',ftsize)
+set(gca,'xtickLabel',{' ',' ',' '})
+set(gca,'XDir','Reverse')
+
+set(findall(gcf,'type','text'),'FontSize',ftsize)
